353699839828ac91cc489fee1338b4c655882a67
Damoon25 <dannilunalp95@gmail.com>
2024-07-31 12:28:19 -0300

    Inicializando repositorio

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <?php include("template/meta.php") ?>
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.14/dist/css/uikit.min.css" />
+    <title>GN Odontología | Home</title>
+</head>
+
+<body>
+
+    <main>
+        <div class="home">
+            <div class="custom-banner mb-1">
+                <div class="container-fluid">
+                    <div class="d-flex justify-content-end align-items-center">
+                        <div class="d-flex">
+                            <a href="https://www.facebook.com/tuperfil" target="_blank" class="text-white mx-2">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                            <a href="https://www.instagram.com/tuperfil" target="_blank" class="text-white mx-2">
+                                <i class="fab fa-instagram"></i>
+                            </a>
+                        </div>
+                        <div class="d-flex align-items-center">
+                            <i class="fa-solid fa-phone"></i>
+                            <a href="tel:+1234567890" class="text-white me-1">+123 456 7890</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="portadaHome position-relative">
+                <?php include("template/header.php") ?>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 align-self-center">
+                            <div class="mb-3 mt-3">
+                                <div class="col-sm-6 col-md-12 mt-3 paddingHome">
+                                    <h1 class="text-center text-md-start animate__fadeInLeft animate__animated animate__faster animate__slower textPrincipalHome">
+                                        En GN Odontología, <br>
+                                        Cuidamos la sonrisa
+                                    </h1>
+                                    <div class="col-sm-12 d-none d-md-flex col-md-5">
+                                        <p class="subText" style="color: var(--text-link) !important;">
+                                            Trabajamos diariamente para ser la mejor opción
+                                            enfocándonos en que la atención
+                                            a nuestros pacientes sea una experiencia placentera.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container mt-5 paddingTop">
+                <div class="row justify-content-center aling-items-center">
+                    <div class="col-md-5 col-sm-12 mb-4">
+                        <div class="card" style=" height: 23rem !important;">
+                            <div class="card-body p-4">
+                                <img src="./public/img/icons/calidad.png" width="20%">
+                                <h3 class="titleSection2" style="color: var(--link) !important;">ODONTOLOGÍA <br> DE CALIDAD</h3>
+                                <p class="subText fw-lighter" style="color: var(--link) !important;">
+                                    Trabajamos diariamente enfocándonos en que la atención a nuestros pacientes sea una experiencia placentera.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-5 col-sm-12 mb-4">
+                        <div class="card" style=" height: 23rem !important;">
+                            <div class="card-body p-4">
+                                <img src="./public/img/icons/profesionales.png" width="20%">
+                                <h3 class="titleSection2" style="color: var(--link) !important;">NUESTROS <br> PROFESIONALES</h3>
+                                <p class="subText fw-lighter" style="color: var(--link) !important;">
+                                    Contamos con profesionales especializados en cada campo de la odontología moderna.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <?php include("template/footer.php") ?>
+    </footer>
+    <script>
+        const text = "GN Odontología ";
+        const speed = 150; // Velocidad de escritura en milisegundos
+        const pauseTime = 1500; // Tiempo de pausa en milisegundos después de completar el texto
+
+        let i = 0;
+        let reverse = false; // Variable para controlar si se está escribiendo o borrando
+
+        function typeWriter() {
+            if (!reverse && i < text.length) {
+                document.getElementById("typedText").innerHTML += text.charAt(i);
+                i++;
+                setTimeout(typeWriter, speed);
+            } else {
+                setTimeout(resetText, pauseTime);
+            }
+        }
+
+        function resetText() {
+            if (!reverse) {
+                reverse = true;
+                setTimeout(typeWriter, speed);
+            } else {
+                if (i > 0) {
+                    document.getElementById("typedText").innerHTML = text.substring(0, i - 1);
+                    i--;
+                    setTimeout(resetText, speed);
+                } else {
+                    reverse = false;
+                    setTimeout(typeWriter, speed);
+                }
+            }
+        }
+
+        window.onload = function() {
+            typeWriter();
+        };
+    </script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.16.14/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.16.14/dist/js/uikit-icons.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.2/dist/umd/popper.min.js" integrity="sha384-q9CRHqZndzlxGLOj+xrdLDJa9ittGte1NksRmgJKeCV9DrM7Kz868XYqsKWPpAmn" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<!-- Control de Responsive Design !-->
+<script>
+    AOS.init();
+</script>
+
+</html>
