004db444df05ef2492aa2e080fd98fc6a55ed5ae
Damoon25 <dannilunalp95@gmail.com>
2024-08-02 15:50:01 -0300

    Commit: actualizacion Contacto

--- profesionales.php
+++ profesionales.php
@@ -109,41 +109,7 @@
                 </div>
             </div>
         </div>
-        <div class="portadaContact">
-            <div class="container">
-                <div class="section col-sm-12 text-center text-md-start justify-content-center justify-content-md-start paddingTop paddingBottom">
-                    <div class="container containerContact">
-                        <div class="row">
-                            <div class="col-sm-12 col-md-4 align-self-start paddingTop paddingBottom mt-2">
-                                <img class="imgContact mb-4 mb-md-0" src="./public/img/imgCovers/IMG-CONTACT.png" alt="Contacto">
-                            </div>
-                            <div class="col-sm-12 col-md-8 d-flex align-items-center">
-                                <div class="container">
-                                    <div class="col-sm-12 text-center text-md-start mt-3">
-                                        <h2 class="titleSection">
-                                            SOLICITA UN TURNO
-                                        </h2>
-                                    </div>
-                                    <p class="subText mb-4" style="color: var(--color3) !important;">
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                        Nobis tempora, maiores commodi possimus facere non,
-                                        deleniti quia quibusdam veniam quod at voluptatum tenetur ratione voluptatem.
-                                        Quisquam assumenda vel dolorum optio.
-                                    </p>
-                                    <div class="col-sm-12 justify-content-end text-end paddingForm mb-3">
-                                        <a href="https://wa.me/1234567890" target="_blank">
-                                            <button type="submit" class="btn2">SOLICITA EL TURNO VIA WHATSAPP
-                                                <i class="ps-1 fab fa-whatsapp" style="color: var(--color2) !important;"></i>
-                                            </button>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php include("contact.php") ?>
     </main>
     <footer>
         <?php include("template/footer.php") ?>
